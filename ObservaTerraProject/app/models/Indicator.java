070a150b846545033ccb76c366dd5feebc388967
paularodriguez <paula.rp93@gmail.com>
2014-04-15 20:45:23 +0200

    Modelo v0

---
+++ ObservaTerraProject/app/models/Indicator.java
@@ -0,0 +1,53 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import com.avaje.ebean.Ebean;
+
+import play.db.ebean.Model;
+
+@Entity
+public class Indicator extends Model {
+    
+  @Id
+  public String code;
+  
+  @Required
+  public String name;
+  
+  public Indicator(String code, String name) {
+	  this.code = code;
+	  this.name = name;
+  }
+   
+  public static Finder<String,Indicator> find = new Finder(String.class, Indicator.class);
+  
+  public static List<Indicator> all() {
+    return find.all();
+  }
+  
+  public static void create(Indicator indicator) {
+	if (Indicator.findByName(indicator.name) == null) { 
+			indicator.save();
+	}
+  }
+  
+  public static void remove(String id) {
+	find.ref(id).delete();
+  }
+  
+  public static void deleteAll() {
+    for (Indicator ind: all()) ind.delete();
+  }
+
+
+  public static Indicator findByName(String name) {
+	  return find.where().eq("name", name).findUnique();
+  }
+
+  public static Indicator findByCode(String code) {
+	  return find.byId(code);
+  }
+
+}
