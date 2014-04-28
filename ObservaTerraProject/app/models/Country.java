f4b9f1e1e91aed73858bea5691d4c24f45a713f2
pablobravo <pablo_bravo3@hotmail.com>
2014-04-28 10:40:31 +0200

    Cambio Pablo Bravo y Alvaro Panizo

--- ObservaTerraProject/app/models/Country.java
+++ ObservaTerraProject/app/models/Country.java
@@ -1,6 +1,5 @@
 package models;
 
-<<<<<<< HEAD
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -57,62 +56,3 @@ public class Country extends Model {
   }
 }
 
-=======
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
-import java.util.*;
-
-import play.db.ebean.Model;
-import play.libs.Json;
-
-@Entity
-public class Country extends Model {
-
-	private static final long serialVersionUID = 1L;
-
-	@Id
-	public String idCountry;
-
-	public String name;
-
-	public Long population;
-
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static Finder<String, Country> find = new Finder(String.class,
-			Country.class);
-
-	public static List<Country> all() {
-		return find.all();
-	}
-
-	public static void create(Country country) {
-		if (Country.findByName(country.name) == null) {
-			country.save();
-		}
-	}
-
-	public static void remove(String idCountry) {
-		find.ref(idCountry).delete();
-	}
-
-	public static void deleteAll() {
-		for (Country c : all())
-			c.delete();
-	}
-
-	public static Country findByName(String name) {
-		return find.where().eq("name", name).findUnique();
-	}
-
-	public static Country findByCode(String idCountry) {
-		return find.byId(idCountry);
-	}
-	
-	public static JsonNode toJson(Country country) {
-		return Json.toJson(country);
-	}
-}
->>>>>>> 739636156d161162212bffc6b5a006e55a441257
--- ObservaTerraProject/app/models/Indicator.java
+++ ObservaTerraProject/app/models/Indicator.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package models;
 
 import java.util.List;
@@ -52,64 +51,4 @@ public class Indicator extends Model {
 	  return find.byId(code);
   }
 
-=======
-package models;
-
-import java.util.List;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-import com.avaje.ebean.Ebean;
-
-import play.data.validation.Constraints.Required;
-import play.db.ebean.Model;
-
-@Entity
-public class Indicator extends Model {
-
-	private static final long serialVersionUID = 1L;
-
-	@Id
-	public String code;
-
-	@Required
-	public String name;
-
-	public Indicator(String code, String name) {
-		this.code = code;
-		this.name = name;
-	}
-
-	public static Finder<String, Indicator> find = new Finder(String.class,
-			Indicator.class);
-
-	public static List<Indicator> all() {
-		return find.all();
-	}
-
-	public static void create(Indicator indicator) {
-		if (Indicator.findByName(indicator.name) == null) {
-			indicator.save();
-		}
-	}
-
-	public static void remove(String id) {
-		find.ref(id).delete();
-	}
-
-	public static void deleteAll() {
-		for (Indicator ind : all())
-			ind.delete();
-	}
-
-	public static Indicator findByName(String name) {
-		return find.where().eq("name", name).findUnique();
-	}
-
-	public static Indicator findByCode(String code) {
-		return find.byId(code);
-	}
-
->>>>>>> 739636156d161162212bffc6b5a006e55a441257
 }
